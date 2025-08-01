8fc07a3ad6772919ca6235db6333aae5d79aefbe
Siam5818 <sihamoudineanzize@gmail.com>
2025-08-01 21:08:15 +0000

    chore(config): ajout des fichiers de configuration Tailwind

---
+++ front_school/postcss.config.js
@@ -0,0 +1,10 @@
+/**
+ * PostCSS configuration file for Tailwind CSS and Autoprefixer
+ * This file is used to process CSS files with PostCSS plugins.
+ */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
