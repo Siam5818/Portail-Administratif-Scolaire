f5aa54040548b93c1a7e128c1b5a8305e02903fd
Siam5818 <sihamoudineanzize@gmail.com>
2025-08-01 21:05:30 +0000

    feat(routing): mise à jour du module de routage principal

--- front_school/src/app/app-routing.module.ts
+++ front_school/src/app/app-routing.module.ts
@@ -1,10 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { authGuard } from './guards/auth.guard';
+import { DashprofComponent } from './components/dashprof/dashprof.component';
+import { DashfamilleComponent } from './components/dashfamille/dashfamille.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent },
+
+  {
+    path: 'change-password',
+    component: ChangePasswordComponent,
+    canActivate: [authGuard],
+  },
+  // Espace administrateur
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard],
+  },
+  // Espace enseignant
+  {
+    path: 'espace-enseignant',
+    component: DashprofComponent,
+    canActivate: [authGuard],
+  },
+  // Espace famille
+  {
+    path: 'espace-famille',
+    component: DashfamilleComponent,
+    canActivate: [authGuard],
+  },
+
+  // Redirection pour les routes non définies
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
